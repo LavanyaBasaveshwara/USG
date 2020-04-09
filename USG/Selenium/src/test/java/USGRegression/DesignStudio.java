ce93373b93b26f66dc6fd52094611aabceacd05b
LavanyaBasaveshwara <mailtolavanyajagadish@gmail.com>
2020-04-09 14:44:53 +0530

    updated the extent report v0.2

--- USG/Selenium/src/test/java/USGRegression/DesignStudio.java
+++ USG/Selenium/src/test/java/USGRegression/DesignStudio.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.WebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import com.aventstack.extentreports.ExtentReports;
@@ -25,57 +25,55 @@ public class DesignStudio {
 	WebDriver driver;
 	ExtentTest logger;
 	ExtentReports extent;
-	
-@BeforeMethod
+
+	@BeforeTest
 	public void setup() {
-		ExtentHtmlReporter reporter = new ExtentHtmlReporter(new File(System.getProperty("user.dir")+"/Reports/newReport.html"));
+		ExtentHtmlReporter reporter = new ExtentHtmlReporter(
+				new File(System.getProperty("user.dir") + "/Reports/newReport.html"));
 		extent = new ExtentReports();
 		extent.attachReporter(reporter);
-		logger=extent.createTest("LoginTest");
-		logger=extent.createTest("EnterDetails");
 	}
-	
-@Test(priority =1)
-public void loginTest() throws IOException
-{
-	
-	driver = DriverSetter.getChromeDriver();
-	driver.get("http://www.google.com");
-	System.out.println("title is "+driver.getTitle());
-	Assert.assertTrue(driver.getTitle().contains("Mukesh"));
-	
-}
 
-@Test(priority =2)
-public void enterDetails() throws IOException
-{
-	driver = DriverSetter.getChromeDriver();
-	driver.get("http://www.google.com");
-	driver.findElement(By.xpath("//input[@name='q']")).sendKeys("Selenium Testing materials");
-	driver.findElement(By.xpath("//div[@class='FPdoLc tfB0Bf']//input[@name='btnK']")).click();
-	Assert.assertTrue(driver.getTitle().contains("pass"));
-	
-}
+	@Test(priority = 1)
+	public void loginTest() throws IOException {
 
-@AfterMethod
-public void tearDown(ITestResult result) throws IOException
-{
-	
-	if(result.getStatus()==ITestResult.FAILURE)
-	{
-		String temp=captureScreenShot.getScreenshot(driver);
-		
-		logger.fail(result.getThrowable().getMessage(), MediaEntityBuilder.createScreenCaptureFromPath(temp).build());
+		logger = extent.createTest("LoginTest");
+		driver = DriverSetter.getChromeDriver();
+		driver.get("http://www.google.com");
+		System.out.println("title is " + driver.getTitle());
+		Assert.assertTrue(driver.getTitle().contains("Mukesh"));
+
 	}
-	
+
+	@Test(priority = 2)
+	public void enterDetails() throws IOException {
+		logger = extent.createTest("EnterDetails");
+		driver = DriverSetter.getChromeDriver();
+		driver.get("http://www.google.com");
+		driver.findElement(By.xpath("//input[@name='q']")).sendKeys("Selenium Testing materials");
+		driver.findElement(By.xpath("//div[@class='FPdoLc tfB0Bf']//input[@name='btnK']")).click();
+		Assert.assertTrue(driver.getTitle().contains("pass"));
+
+	}
+
+	@AfterMethod
+	public void tearDown(ITestResult result) throws IOException {
+
+		if (result.getStatus() == ITestResult.FAILURE) {
+			String temp = captureScreenShot.getScreenshot(driver);
+
+			logger.fail(result.getThrowable().getMessage(),
+					MediaEntityBuilder.createScreenCaptureFromPath(temp).build());
+		}
+
 //	extent.flush();
-	driver.quit();
-	
-}
+		driver.quit();
 
-@AfterTest
-public void flushTest() {
-	extent.flush();
-}
+	}
+
+	@AfterTest
+	public void flushTest() {
+		extent.flush();
+	}
 
 }
